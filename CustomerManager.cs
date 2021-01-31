dd7629a293e75221d9f1a555b865c34133de3725
bdedeogluu <62393670+bdedeogluu@users.noreply.github.com>
2021-01-31 12:35:29 +0300

    Add files via upload

---
+++ CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine("Customer is added. Properties of customer are:\n" + "Full Name: " + customer.CustomerName + " " + customer.CustomerSurname +
+                "\nCustomer Id: " + customer.CustomerId + "\t Customer Phone: " + customer.CustomerPhone);
+        }
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine("Customer is deleted. Properties of customer were:\n" + "Full Name: " + customer.CustomerName + " " + customer.CustomerSurname +
+                "\nCustomer Id: " + customer.CustomerId + "\t Customer Phone: " + customer.CustomerPhone);
+        }
+        public void ListCustomer(Customer[] customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Customer Id: " + customer.CustomerId);
+                Console.WriteLine("Customer Full Name: " + customer.CustomerName + " " + customer.CustomerSurname);
+                Console.WriteLine("Customer Phone: " + customer.CustomerPhone);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.CustomerId = 1222;
+            customer1.CustomerName = "John";
+            customer1.CustomerSurname = "Angle";
+            customer1.CustomerPhone = "23546";
+
+            Customer customer2 = new Customer();
+            customer2.CustomerId = 1224;
+            customer2.CustomerName = "Micheal";
+            customer2.CustomerSurname = "Key";
+            customer2.CustomerPhone = "98654";
+
+            Customer customer3 = new Customer();
+            customer3.CustomerId = 1226;
+            customer3.CustomerName = "Jiso";
+            customer3.CustomerSurname = "Frost";
+            customer3.CustomerPhone = "56423";
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.ListCustomer(customers);
+            customerManager.AddCustomer(customer1);
+            customerManager.DeleteCustomer(customer2);
+        }
+    }
+}
